52f7401b75efdb8b966279408d9f280a325c12a6
Mike Lyons <mlyons@apple.com>
2013-08-07 18:25:57 -0700

    Initial Commit

---
+++ runc.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+TMPFILE=`mktemp '/tmp/runc.XXXXXXXXX'`
+
+mv $TMPFILE "$TMPFILE.c"
+TMPFILE="$TMPFILE.c"
+
+echo "#include <stdio.h>" > $TMPFILE
+echo "int main( int argc, char *argv[] ) {" >> $TMPFILE
+
+while read -r line
+do 
+	echo -n "$line" >> $TMPFILE; 
+done
+
+echo "}" >> $TMPFILE;
+
+clang -o /tmp/runc $TMPFILE
+rm $TMPFILE
+/tmp/runc
